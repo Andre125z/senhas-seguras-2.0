d978f4c70b694e414d3209e6779dc8ea2ff89be1
Andre125z <andre.casapula@escola.pr.gov.br>
2026-06-14 22:35:23 -0300

    ..

--- main.js
+++ main.js
@@ -6,12 +6,18 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
-function diminuiTamanho(){ 
-    tamanhoSenha = tamanhoSenha-1;
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha-1;
+
+    }
     numeroSenha.textContent = tamanhoSenha
 }
 function aumentaTamanho(){
-    tamanhoSenha = tamanhoSenha+1;
+    if (tamanhoSenha<50){
+        tamanhoSenha = tamanhoSenha+1;
+
+    }
     numeroSenha.textContent = tamanhoSenha;
 }
 
